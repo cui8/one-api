5ad897470533478eb094cc3935f8186588fe7404
cui8 <70044239+cui8@users.noreply.github.com>
2024-07-19 11:55:46 +0800

    Update index.js

--- web/default/src/pages/Docs/index.js
+++ web/default/src/pages/Docs/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Docs = () => {
   return (
     <iframe
-      src="https://docs.ddf.gay"
+      src="https://docs.ddf.gay/pages/openai/%E4%B8%80%E3%80%81OpenAI%20ChatGPT/%E5%BF%AB%E9%80%9F%E6%8E%A5%E5%85%A5.html"
       style={{ width: '100%', height: '100vh', border: 'none' }}
     />
   );
